f30ff100aa60636bbbf97a0df6f8ccd484bbee1b
Shari Hoch <Shari333444555@gmail.com>
2024-06-21 14:31:01 -0400

    GUI comments Shari and Luis

--- simpleMovie.py
+++ simpleMovie.py
@@ -130,13 +130,17 @@ def is_valid_youtube_url(url):
 def create_playlist():
     video_entries = []
 
+    # Defines the contents of the window
+
     layout = [
         [sg.Text('LOOPY Playlist Creator')],
         [sg.Text('Playlist Name:'), sg.InputText(key='-PLAYLIST_NAME-')],
         [sg.Text('Select Video Source (max 10 entries):')]
     ]
-
+    
+    # Defines more contents of the window (namely, the reocurring features)
     # Add input fields for up to 10 videos
+
     for i in range(10):
         layout.extend([
             [sg.Text(f'Video {i+1} Source:'), 
@@ -148,6 +152,8 @@ def create_playlist():
 
     layout.append([sg.Button('Submit'), sg.Button('Cancel')])
 
+    # Creates the window
+
     window = sg.Window('Video Playlist Creator', layout, finalize=True)
 
     while True:
@@ -155,22 +161,29 @@ def create_playlist():
 
         if event in (sg.WINDOW_CLOSED, 'Cancel'):
             break
+            # The 'Cancel' button causes the program to break
         elif event == 'Submit':
             playlist_name = values['-PLAYLIST_NAME-']
             video_entries = []
             error = False
-            for i in range(10):
+            for i in range(10): # 'For all ten possible entries in the playlist:'
                 if values[f'-YOUTUBE-{i}'] and values[f'-YOUTUBE_URL-{i}']:
+                    # If a playlist entry is not empty and contains a YouTube URL:
                     url = values[f'-YOUTUBE_URL-{i}']
                     if is_valid_youtube_url(url):
                         video_entries.append(('YouTube', url))
+                        # If the YouTube URL is valid, append the entry to the Playlist list
                     else:
                         sg.popup_error(f'Invalid YouTube URL for Video {i+1}: {url}')
                         error = True
                         break
+                        # If the YouTube URL is invalid, create a error pop-up and break 
+                        # out of the create playlist window
                 elif values[f'-LOCAL-{i}'] and values[f'-LOCAL_PATH-{i}']:
                     path = values[f'-LOCAL_PATH-{i}']
                     video_entries.append(('Local', path))
+                     # If a playlist entry is not empty and a directory pathway, append the entry
+
 
             if not error:
                 # Handle the collected data here
