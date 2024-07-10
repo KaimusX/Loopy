d7cbb1d15cedd4b57e3568f14915b26909b1d6be
mjl036 <94404566+mjl036@users.noreply.github.com>
2024-07-10 10:40:17 -0400

    Added 3 strike lockout

--- UserLogin.py
+++ UserLogin.py
@@ -7,8 +7,8 @@ import TwoFactor
 from PIL import Image, ImageTk
 import Home_Page
 
-TEMP_OTP = None
-
+counter = 0
+last_usr = ''
 class SimpleAccountManagerUI:
     def __init__(self, master):
         self.master = master
@@ -42,18 +42,30 @@ class SimpleAccountManagerUI:
         tk.Button(self.main_frame, text="Back", command=self.go_back).pack(pady=10)
 
     def login(self):
+        global counter
+        global last_usr
         username = self.username_entry.get()
         password = self.password_entry.get()
 
         if not InputValidation.validate_username(username):         
-            messagebox.showerror("Login", "Invalid username.")      
-        elif not InputValidation.password_check(password):          
-            messagebox.showerror("Login", "Invalid Password.")      
+            messagebox.showerror("Login", "Invalid username.")
 
-        if UsersDatabase.UserAccount.checkUser(username, password) == "Success":
-            self.show_2fa_screen(username) # Go to 2FA to check it, then we can say success and let them through. We can maybe put the success on it's own function.
+        if last_usr != self.username_entry.get():
+            counter = 0
+
+        last_usr = username
+
+        if counter == 3:
+            messagebox.showerror("Login", "Too many attempts. Try again later.")
+        elif UsersDatabase.UserAccount.checkUser(username, password) == "Success":
+            self.master.destroy()
+            Home_Page.Home(username)
+            #self.show_2fa_screen(username) # Go to 2FA to check it, then we can say success and let them through. We can maybe put the success on it's own function.
+        elif UsersDatabase.UserAccount.checkUser(username, password) == "Invalid":
+            counter += 1
+            messagebox.showerror("Login", "Incorrect password.")
         else:
-            messagebox.showerror("Login", "Username or password not found.")
+            messagebox.showerror("Login", "Username not found.")
 
     def show_register_screen(self):
         self.clear_frame()
@@ -78,7 +90,7 @@ class SimpleAccountManagerUI:
         tk.Button(self.main_frame, text="Register", command=self.register).pack(pady=10)
         tk.Button(self.main_frame, text="Back", command=self.go_back).pack(pady=10)
 
-    def finishRegister(self):
+    def register(self):
         name = self.name_entry.get()
         email = self.email_entry.get()
         username = self.username_entry.get()
@@ -90,47 +102,13 @@ class SimpleAccountManagerUI:
             messagebox.showerror("Registration", "Invalid email.")              
         elif not InputValidation.password_check(password):                      
             messagebox.showerror("Registration", "Invalid Password.")           
-
         else:
-            global TEMP_OTP
             try:
-                UsersDatabase.UserAccount.createUserRow(username, name, email, password, TEMP_OTP)
+                UsersDatabase.UserAccount.createUserRow(username, name, email, password)
                 messagebox.showinfo("Register", "Registration successful!")
                 self.create_widgets()
             except ValueError as e:
                 messagebox.showerror("Register", str(e))
-            cwd = os.getcwd()
-            qr_path = os.path.join(cwd, 'qr.png')
-            os.remove(qr_path)
-            TEMP_OTP = None
-        
-
-    def register(self):
-        email = self.email_entry.get()
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-
-        if not InputValidation.validate_username(username):                     
-            messagebox.showerror("Registration", "Invalid username.")           
-        elif not InputValidation.is_valid_email(email):                         
-            messagebox.showerror("Registration", "Invalid email.")              
-        elif not InputValidation.password_check(password):                      
-            messagebox.showerror("Registration", "Invalid Password.")           
-        else:
-            global TEMP_OTP
-            TEMP_OTP = TwoFactor.otp_gen(username)
-
-            cwd = os.getcwd()
-            qr_path = os.path.join(cwd, 'qr.png')
-            
-            image1 = Image.open(qr_path)
-            test = ImageTk.PhotoImage(image1)
-            label1 = tk.Label(image = test)
-            label1.image = test
-            
-            label1.place(x=550, y=70)
-            
-            tk.Button(self.main_frame, text="Done", command=self.finishRegister).pack(pady=10)
 
     def show_account_screen(self, user_info):
         self.clear_frame()
@@ -141,32 +119,7 @@ class SimpleAccountManagerUI:
         tk.Label(self.main_frame, text=f"Username: {user_info['username']}").pack()
 
         tk.Button(self.main_frame, text="Logout", command=self.create_widgets).pack(pady=10)
-#####################################################################################################################################
 
-    def show_2fa_screen(self,usr):
-        self.clear_frame()
-        self.screen_stack.append(self.show_2fa_screen)  # Push the current screen function to the stack
-
-        tk.Label(self.main_frame, text="2-Factor Authentication").pack(pady=10)
-        tk.Label(self.main_frame, text="Enter the 2FA code sent to you:").pack()
-        self.otp_entry = tk.Entry(self.main_frame)
-        self.otp_entry.pack(pady=10)
-
-        tk.Button(self.main_frame, text="Submit", command=self.verify_2fa(usr)).pack(pady=10)
-        tk.Button(self.main_frame, text="Back", command=self.go_back).pack(pady=10)
-
-    def verify_2fa(self,usr):
-        otp_code = self.otp_entry.get()
-        totp = UsersDatabase.UserAccount.getOTP(usr)
-        if True: #TwoFactor.verify_otp(totp, otp_code):
-            messagebox.showinfo("2FA", f"2FA successful! You are successfully logged in {usr}!")
-            self.create_widgets()
-            self.master.destroy()
-            Home_Page.Home(usr)
-        else:
-            messagebox.showerror("2FA", "Invalid 2FA code. Please try again.")
-
-#####################################################################################################################################
     def go_back(self):
         if self.screen_stack:
             self.screen_stack.pop()  # Remove the current screen function
